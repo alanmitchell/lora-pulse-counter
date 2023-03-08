27c07d931585cbd311b2e132e199c8aeb7903173
Alan Mitchell <tabb99@gmail.com>
2023-03-08 14:15:45 -0900

    Changed to Digital Pin Names.

--- config_pin.example.py
+++ config_pin.example.py
@@ -6,4 +6,6 @@ import board
 # The list of pin names to use for pulse inputs.  Any number of pins can
 # be accommodated.  3 bytes per pin will be transmitted by LoRaWAN plus a one byte
 # message type.  So, only 3 pins can fit within the SF10 11-byte US limit.
-PINS = [board.A0, board.A1]
+
+PINS = [board.D0, board.D1]     # 2 pulse inputs
+# PINS = [board.D0, board.D1, board.D2]        # 3 pulse inputs
